d550f812b2a52965b4934e7a58042371d35df973
Paulo_Scarano <scaranopaulo@gmail.com>
2022-07-17 10:33:07 -0300

    Modulo 9 - #5 CSS e JS com Hooks

--- wp-content/themes/primeirotema/functions.php
+++ wp-content/themes/primeirotema/functions.php
@@ -1,6 +1,9 @@
 <?php
-//Include
-require get_template_directory().'/include/pp_footer_functions.php';
+function pp_theme_styles()
+{
+    wp_enqueue_style('theme_css', get_template_directory_uri() . '/assets/css/theme.css');
+    wp_enqueue_script('theme_js', get_template_directory_uri() . '/assets/js/script.js', array('jquery'), '', true);
+}
 
 //Hooks
-add_action('shutdown', 'pp_fim');
+add_action('wp_enqueue_scripts', 'pp_theme_styles');
--- wp-content/themes/primeirotema/index.php
+++ wp-content/themes/primeirotema/index.php
@@ -1,1 +1,15 @@
-<h1>Meu Primeiro Tema</h1>
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <?php wp_head(); ?>
+</head>
+
+<body>
+    <h1>Meu Primeiro Tema</h1>
+
+
+    <?php wp_footer(); ?>
+</body>
+
+</html>
