b37e79c6be6074927e357982710a3589ecd25f95
Paulo_Scarano <scaranopaulo@gmail.com>
2022-07-26 20:07:34 -0300

    Modulo 9 - #14 Página única e 404

---
+++ wp-content/themes/primeirotema/404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+<section>
+    <div class="container">
+        <h1>Página não encontrada</h1>
+        <p>A página que você está procurando não foi encontrada.</p>
+    </div>
+
+
+    <div style="clear:both"></div>
+</section>
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/primeirotema/page.php
@@ -0,0 +1,36 @@
+<?php get_header(); ?>
+<section>
+    <div class="container">
+        <?php while (have_posts()) : ?>
+            <?php the_post(); ?>
+
+            <article>
+
+
+                <h2><?php the_title(); ?></h2>
+
+                <p>
+                    <?php the_content(); ?>
+                </p>
+
+                <p>
+                    <?php comments_number('0 comentários', 'um comentário', '% comentários'); ?>
+                </p>
+
+                <hr />
+
+                <?php
+                if (comments_open()) {
+                    comments_template();
+                }
+                ?>
+            </article>
+        <?php endwhile; ?>
+    </div>
+
+    <?php get_sidebar() ?>
+
+    <div style="clear:both"></div>
+</section>
+
+<?php get_footer(); ?>
