43c7fd2acde9304d8d6f3c767c2d073078d95285
Paulo_Scarano <scaranopaulo@gmail.com>
2022-08-05 20:50:02 -0300

    Modulo 9 - #19 Template com Bootstrap 1/4

---
+++ wp-content/themes/segundoTema/include/setup.php
@@ -0,0 +1,18 @@
+<?php
+
+function ps_theme_styles()
+{
+    wp_enqueue_style("bootstrap", get_template_directory_uri() . "/vendor/bootstrap/css/bootstrap.min.css");
+    wp_enqueue_style("fontawesome", get_template_directory_uri() . "/vendor/font-awesome/css/font-awesome.min.css");
+    wp_enqueue_style("googleLora", "https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic");
+    wp_enqueue_style("googleOpenSans", "https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800");
+    wp_enqueue_style("cleanblog", get_template_directory_uri() . "/css/clean-blog.min.css");
+
+    wp_enqueue_script("jquery", get_template_directory_uri() . "/vendor/jquery/jquery.min.js");
+    wp_enqueue_script("Bootstrap_js", get_template_directory_uri() . "/vendor/bootstrap/js/bootstrap.bundle.min.js");
+    wp_enqueue_script("cleanblog_js", get_template_directory_uri() . "/js/clean-blog.min.js");
+}
+
+function ps_after_setup()
+{
+}
